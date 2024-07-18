407fb35f55933769ea2b7042161deaa8f1a67b8d
VitusPl <victorplug@gmail.com>
2024-07-18 19:12:24 +0800

    telephon active

--- src/components/AppNav.vue
+++ src/components/AppNav.vue
@@ -18,7 +18,7 @@
     </div>
 
     <div class="text-sm text-white">
-      <span class="text-base text-blue-700 font-bold underline decoration-2">
+      <span class="text-sm text-blue-700 font-bold underline decoration-2">
         <a href="tel:+79041421188">+7(904) 142-11-88</a>
       </span>
       <br>

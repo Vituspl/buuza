a96047f96e0bc99f57f92b1faf4810e4c1a4ba58
VitusPl <victorplug@gmail.com>
2024-07-18 19:06:08 +0800

    telephon active

--- src/components/AppNav.vue
+++ src/components/AppNav.vue
@@ -13,12 +13,12 @@
       </button>
     </router-link>-->
 
-    <div class="text-base text-white">
+    <div class="text-sm text-white">
       Наш адрес: г. Ангарск , <br> 33-й микрорайон, 14
     </div>
 
-    <div class="text-base text-white">
-      <span class="text-lg text-blue-700 font-bold underline decoration-2">
+    <div class="text-sm text-white">
+      <span class="text-base text-blue-700 font-bold underline decoration-2">
         <a href="tel:+79041421188">+7(904) 142-11-88</a>
       </span>
       <br>
